786b9a4be78e116b5ef605bfb141adaa2cdba701
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-21 22:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// script.js
 function getFormvalue() {
-    //Write your code here
+  var firstName = document.getElementById('fname').value;
+  var lastName = document.getElementById('lname').value;
 
+  alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
 }
