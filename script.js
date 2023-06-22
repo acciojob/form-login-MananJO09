a2b144694b03c5c5bd5d42c6818e5673eec2a4f4
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-22 23:12:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // script.js
 function getFormvalue() {
-  let name = ""
+   var x=document.getElementById("form1");
+    let name = ""
     for (var i=0;i<x.length;i++) {
         if (x.elements[i].value!='Submit')
             name += x.elements[i].value + " "
